b69f0cfc633d57123c784a200f962156dd570bf2
Theresa Eliger <910theresa@gmail.com>
2018-08-28 18:08:27 -0700

    First commit.

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>My name is Theresa and this is my first Gatsby site! :)</h1>
+    <p>I can't wait to start building with the JAMstack.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
